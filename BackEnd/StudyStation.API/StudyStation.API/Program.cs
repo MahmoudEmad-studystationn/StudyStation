896e4e5ce0093424021ac3a4557fbaecc64e8f3e
Mahmoud-code1 <mahmoud.emad.studystation@gmail.com>
2026-04-21 19:43:47 +0200

    logout changes

--- BackEnd/StudyStation.API/StudyStation.API/Program.cs
+++ BackEnd/StudyStation.API/StudyStation.API/Program.cs
@@ -98,7 +98,7 @@ builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.ujknkj
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
